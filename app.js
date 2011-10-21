5c07ae7aebea15cda7be103e1745d9e495521d09
BlueHotDog <dannifriedland@gmail.com>
2011-10-21 02:00:47 +0200

    more testing

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = require('express').createServer();
-var sys = require("sys");
-var GitHubApi = require("github").GitHubApi;
-
-var github = new GitHubApi(true);
+var app = require('express').createServer(),
+	sys = require("sys"),
+	github = new (require("github").GitHubApi)(true),
+	Canvas = require('canvas'),
+	fs = require('fs');
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.send('<img src="' + generateScore("hello there danni") + '" />');
 });
 
 app.get('/info/:uid/:repo.:format', function(req,res) {
@@ -18,12 +18,38 @@ app.get('/info/:uid/:repo.:format', function(req,res) {
 			console.log
 		});
 	} else {
-		next();
-	}
+		next(); }
 });
-
 var repoInfo = {
 	
+}
+
+
+function generateScore(t) {
+	var Canvas = require('canvas'),
+		Image = Canvas.Image,
+		canvas = new Canvas((14+5)*5+50, 14),
+		ctx = canvas.getContext('2d'),
+		fs = require('fs');
+
+	var textToWrite = t;
+	ctx.font = 'normal 14px Impact, serif';
+	ctx.fillStyle = '#FF7C0C';
+	var initialStar = ctx.measureText(textToWrite).width;
+	while(initialStar>50) {
+		textToWrite =  textToWrite.slice(0,textToWrite.length-1);
+		initialStar = ctx.measureText(textToWrite).width;
+	}
+	ctx.fillText(textToWrite, 0, 14, 50);
+	console.log(initialStar);
+	var star = fs.readFileSync(__dirname + '/images/star.png');
+
+	var img = new Image;
+	img.src = star;
+	for(var i=0;i<5;i++) {
+		ctx.drawImage(img, initialStar+i*14+5*i, 0);
+	}
+	return canvas.toDataURL();	
 }
 function getRepoInfo(uid,repo,format, cb) {
 	github.getRepoApi().show(uid,repo, function(err, info) {
@@ -31,5 +57,7 @@ function getRepoInfo(uid,repo,format, cb) {
 	})
 }
 
-getRepoInfo("BlueHotDog", "contacts");
-// app.listen(3000);
+
+// getRepoInfo("BlueHotDog", "contacts");
+
+app.listen(3000);
